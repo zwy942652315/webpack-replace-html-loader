2ad75fea02a6b753e399bd8c4eb1f68de72846e6
zwy942652315 <942652315@qq.com>
2020-06-01 01:39:02 +0800

    update 上传效果图

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <el-container style="height: 500px; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1', '3']">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>用户管理</template>
+            <el-menu-item-group>
+              <template slot="title">用户列表</template>
+              <el-menu-item index="1-1">用户列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <span>王小虎</span>
+        </el-header>
+        
+        <el-main>
+          <div class="list-title">展示html模板内容</div>
+          <userList />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import userList from './components/user-list';
+export default {
+  components: {
+    userList,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+.el-aside {
+  color: #333;
+}
+.list-title {
+  margin: 30px auto;
+}
+</style>
